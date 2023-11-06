d9eea5e44509f80b3481865dd0591adfb8ac360c
whashim <whashim@gmail.com>
2023-11-07 06:54:34 +0800

    First commit

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,16 @@
+<template><h1>hello oe 2022 </h1>
+<div>
+    <button @click="$event => count++">+</button> {{ count }}
+    <button @click="$event => count--">-</button>
+</div>
+</template>
+
+<script>
+export  default {
+    data() {
+        return {
+            count: 0 ,
+        };
+    },
+};
+</script>
